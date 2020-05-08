56d9b225e757c14cb3d1561e26d0bf1365bf5416
Hetsh <git@hetsh.de>
2020-05-08 21:23:54 +0200

    Import

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+FROM library/alpine:20200428
+RUN apk add --no-cache \
+    mumble=1.18.0-r0
+
+# App user
+ARG OLD_USER="mumble"
+ARG APP_USER="http"
+ARG	OLD_UID=100
+ARG	APP_UID=33
+ARG OLD_GROUP="mumble"
+ARG APP_GROUP="http"
+ARG	OLD_GID=101
+ARG	APP_GID=33
+RUN sed -i "/:$APP_UID/d" /etc/passwd && \
+    sed -i "s|$OLD_USER:x:$OLD_UID:$OLD_GID|$APP_USER:x:$APP_UID:$APP_GID|" /etc/passwd && \
+    sed -i "/:$APP_GID/d" /etc/group && \
+    sed -i "s|$OLD_GROUP:x:$OLD_GID:$OLD_USER|$APP_GROUP:x:$APP_GID:|" /etc/group
+
+# Volumes
+ARG CONF_DIR="/etc/mumble/modules"
+ARG HOSTS_DIR="/etc/mumble/conf.d"
+ARG SRV_DIR="/srv"
+ARG LOG_DIR="/var/log/mumble"
+RUN chown -R "$APP_USER":"$APP_GROUP" "$HOSTS_DIR" "$SRV_DIR" "$LOG_DIR"
+VOLUME ["$CONF_DIR", "$HOSTS_DIR", "$SRV_DIR", "$LOG_DIR"]
+
+# Configuration
+ARG RUN_DIR="/run/mumble"
+RUN ln -s "$LOG_DIR" "$APP_DIR/logs" && \
+    mkdir "$RUN_DIR" && \
+    chown -R "$APP_USER":"$APP_GROUP" "$RUN_DIR" && \
+    sed -i "s|user $OLD_USER|user $APP_USER|" /etc/mumble/mumble.conf && \
+    sed -i "s|group $OLD_GROUP|group $APP_GROUP|" /etc/mumble/mumble.conf
+
+#      HTTP   HTTPS
+EXPOSE 80/tcp 443/tcp
+
+WORKDIR "$SRV_DIR"
+ENTRYPOINT ["mumble", "-g", "daemon off;"]
---
+++ build.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+
+# Abort on any error
+set -e -u
+
+# Simpler git usage, relative file paths
+CWD=$(dirname "$0")
+cd "$CWD"
+
+# Load helpful functions
+source libs/common.sh
+
+# Check access to docker daemon
+assert_dependency "docker"
+if ! docker version &> /dev/null; then
+	echo "Docker daemon is not running or you have unsufficient permissions!"
+	exit -1
+fi
+
+# Build the image
+APP_NAME="mumble"
+docker build --tag "$APP_NAME" .
+
+if confirm_action "Test image?"; then
+	# Set up temporary directory
+	TMP_HOSTS_DIR=$(mktemp -d "/tmp/$APP_NAME-HOSTS-XXXXXXXXXX")
+	add_cleanup "rm -rf $TMP_HOSTS_DIR"
+	echo "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	root /srv;
+	index index.html;
+
+	server_name _;
+
+	location / {
+		try_files \$uri \$uri/ =404;
+	}
+}" > "$TMP_HOSTS_DIR/test.conf"
+
+	TMP_SRV_DIR=$(mktemp -d "/tmp/$APP_NAME-SRV-XXXXXXXXXX")
+	add_cleanup "rm -rf $TMP_SRV_DIR"
+	echo "<html>
+	<head>
+		<title>Test</title>
+	</head>
+	<body>
+		<h1>Success!</h1>
+	</body>
+</html>" > "$TMP_SRV_DIR/index.html"
+
+	# Apply permissions, UID & GID matches process user
+	extract_var APP_UID "./Dockerfile" "\K\d+"
+	extract_var APP_GID "./Dockerfile" "\K\d+"
+	chown -R "$APP_UID":"$APP_GID" "$TMP_HOSTS_DIR" "$TMP_SRV_DIR"
+
+	# Start the test
+	extract_var HOSTS_DIR "./Dockerfile" "\"\K[^\"]+"
+	extract_var SRV_DIR "./Dockerfile" "\"\K[^\"]+"
+	docker run \
+	--rm \
+	--interactive \
+	--publish 80:80/tcp \
+	--publish 443:443/tcp \
+	--mount type=bind,source="$TMP_HOSTS_DIR",target="$HOSTS_DIR" \
+	--mount type=bind,source="$TMP_SRV_DIR",target="$SRV_DIR" \
+	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+	--name "$APP_NAME" \
+	"$APP_NAME"
+fi
